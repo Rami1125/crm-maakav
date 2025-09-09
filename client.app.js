93c01ea7466133b173e2047e9074437380c7c888
Rami1125 <rami.msarwa1@gmail.com>
2025-09-09 21:53:36 +0300

    Update client.app.js

--- client.app.js
+++ client.app.js
@@ -7,7 +7,7 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     // ========== STATE & CONFIG ==========
-    const API_URL = "https://script.google.com/macros/s/AKfycbwkwzl-7S0KT77As4kCfciwvrIZnAlfwW-6_U4kXBmxmUlktotTSwyWo-s8q585CsOQFg/exec";
+    const API_URL = "https://script.google.com/macros/s/AKfycbxtQtoo6MQ2xBeJ3MLUa_e2rA4zPHKEXlHLBSzx5MSJMcLoql0vyHeC_yLGfIGinKjEVw/exec";
     
     const CHAT_TEMPLATES = [
         "המכולה מלאה, אשמח לתאם פינוי.",
